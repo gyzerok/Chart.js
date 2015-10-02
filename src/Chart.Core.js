6d88f9e2207b7a6c8da84d018435f2f4cf0d697d
Fedor Nezhivoy <gyzerok@gmail.com>
2015-10-03 05:15:33 +0600

    fix bug, new build

--- src/Chart.Core.js
+++ src/Chart.Core.js
@@ -1592,7 +1592,7 @@
 			this.xLabels.pop();
 			this.valuesCount--;
 			this.fit();
-		}
+		},
 		// Fitting loop to rotate x Labels and figure out what fits there, and also calculate how many Y steps to use
 		fit: function(){
 			// First we need the width of the yLabels, assuming the xLabels aren't rotated
